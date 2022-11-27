a63b6e0f43d24e25a9ca9ed9069e854666416681
Sheila Strahan <smstrahan@gmail.com>
2022-11-27 16:59:19 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentLog from './StudentLog.js';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentLog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StudentLog.js
@@ -0,0 +1,9 @@
+function StudentLog() {
+    return (
+      <div className="studentlog">
+        This is the student log page
+      </div>
+    );
+  }
+  
+  export default StudentLog;
