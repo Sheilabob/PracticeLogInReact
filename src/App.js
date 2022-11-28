d738a32eff298d1097b05be34068208726940b20
Sheila Strahan <smstrahan@gmail.com>
2022-11-28 16:35:38 -0700

    made rendering of student and teacher logs contigent on login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StudentLog from './StudentLog.js';
-import TeacherLog from './TeacherLog.js';
 import LoginPage from './Login';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
-      <StudentLog />
-      <TeacherLog />
 
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import StudentLog from './StudentLog.js';
+import TeacherLog from './TeacherLog.js';
 
 function LoginPage() {
+    let logSheet;
     const [val, setVal] = useState("Please log in")
     const click = () => {
         alert(val)
@@ -8,11 +11,19 @@ function LoginPage() {
     const getValue =(entry) => {
         setVal(entry.target.value)
     }
+    if (val === 'student') {
+        logSheet = <StudentLog />
+    } else if (val === 'teacher') {
+        logSheet = <TeacherLog />
+    } else {
+        logSheet = 'Please sign in'
+    }
     return (
     <div>
         <input type="text" value={val} onChange={getValue} />
         <button onClick={click}>Login</button>
-        <div>{val}</div>
+        <div>{logSheet}</div>
+
     </div>
     )
 
