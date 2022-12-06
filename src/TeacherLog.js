150ecee6cac681923440acbda73a91f26dec347f
Sheila Strahan <smstrahan@gmail.com>
2022-12-05 18:24:11 -0700

    started teacher dropdown for student select

--- src/TeacherLog.js
+++ src/TeacherLog.js
@@ -1,7 +1,16 @@
 function TeacherLog() {
     return (
       <div className="teacherlog card">
-        This is the Teacher log page
+        <h1>Teacher Assigment Page</h1>
+        <form>
+          <label for="students">Select Student</label>
+          <select id="students" name="students">
+            <option value="Grace">Grace</option>
+            <option value="Alice">Alice</option>
+            <option value="Ella">Ella</option>
+            <option value="Liam">Liam</option>
+          </select>
+        </form>
       </div>
     );
   }
