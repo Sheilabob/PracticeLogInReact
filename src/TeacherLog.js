bbf572fac9f6648d0614182d1b911e2da0738347
Sheila Strahan <smstrahan@gmail.com>
2022-12-06 18:05:22 -0700

    start outlining different teacher sections

--- src/TeacherLog.js
+++ src/TeacherLog.js
@@ -2,7 +2,11 @@ function TeacherLog() {
     return (
       <div className="teacherlog card">
         <h1>Teacher Assigment Page</h1>
+        <div className="card">
+          <h2>Full Report</h2>
+        </div>
         <form>
+          <h2>Individual Student Management</h2>
           <label for="students">Select Student</label>
           <select id="students" name="students">
             <option value="Grace">Grace</option>
