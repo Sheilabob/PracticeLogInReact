8d2b237da463f47da84ae460d1b83b1dcd7c1d32
Sheila Strahan <smstrahan@gmail.com>
2022-12-06 18:56:23 -0700

    tabs are reactive

--- src/Components/TabComponent/Tabs.js
+++ src/Components/TabComponent/Tabs.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import FirstTab from "../AllTabs/FirstTab";
+import SecondTab from "../AllTabs/SecondTab";
+
+
 
 const Tabs = () => {
-  return (
+    const handleTab1 = () => {
+        // update the state to tab1
+        setActiveTab("tab1");
+      };
+      const handleTab2 = () => {
+        // update the state to tab2
+        setActiveTab("tab2");
+      };
+  const [activeTab, setActiveTab] = useState("tab1");
+   return (
     <div className="Tabs">
       {/* Tab nav */}
       <ul className="nav">
-        <li>Tab 1</li>
-        <li>Tab 2</li>
+        <li className={activeTab === "tab1" ? "active" : ""} onClick={handleTab1}>Tab 1</li>
+        <li className={activeTab === "tab2" ? "active" : ""} onClick={handleTab2}>Tab 2</li>
       </ul>
       <div className="outlet">
-        {/* content will be shown here */}
+        {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
       </div>
     </div>
   );
