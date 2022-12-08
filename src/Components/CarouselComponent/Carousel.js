c01390a7e72448e999a56f3932db5c8c8ae3a87e
Sheila Strahan <smstrahan@gmail.com>
2022-12-08 16:52:30 -0700

    put content in carousel

--- src/Components/CarouselComponent/Carousel.js
+++ src/Components/CarouselComponent/Carousel.js
@@ -1,7 +1,29 @@
 import { useEffect, useState } from "react";
 
 function Carousel() {
-  const data = ["1", "2", "3", "4", "5"]
+  let div1 = <div className="Assignment"><h3>Ear Training</h3>
+  <p>Listen to songs up through Cuckoo</p>
+  <p>Notes:</p>
+  <p>Put in radio box for repetition or minutes</p></div>
+  let div2 = <div className="Assignment"><h3>Sight Reading</h3>
+  <p>The Pony Song, p. 24</p>
+  <p>Notes: Pay attention to quarter rests.</p>
+  <p>Put in radio box for repetition or minutes</p></div>
+  let div3 = <div className="Assignment"><h3>Main Practice Piece</h3>
+  <p>Honeybee, RH and then LH, separately</p>
+  <p>Notes: Pay attention to quarter rests.</p>
+  <p>Put in radio box for repetition or minutes</p></div>
+  let div4 = <div className="Assignment"><h3>Review</h3>
+  <p>Mississippi Hop Frog, Ice Cream Cone, Run Pony, Twinkle</p>
+  <p>Notes: Pick one per hand per day, rotate through all at least once a week.</p>
+  <p>Put in radio box for repetition or minutes</p></div>
+  let div5 = <div className="Assignment"><h3>Preview</h3>
+  <p>Cuckoo</p>
+  <p>Notes: Starts on G in RH with finger 5 - play as far as you can sound out</p>
+  <p>Put in radio box for repetition or minutes</p></div>
+
+
+  const data = [div1, div2, div3, div4, div5]
   const [currentIndex, setCurrentIndex] = useState(0)
   const carouselInfiniteScroll = () => {
     if (currentIndex === data.length-1) {
