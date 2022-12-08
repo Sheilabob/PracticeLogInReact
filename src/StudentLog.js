250bda7f3cce1ed0f013e27a89e476b1cf1e0158
Sheila Strahan <smstrahan@gmail.com>
2022-12-07 19:08:10 -0700

    started student carousel

--- src/StudentLog.js
+++ src/StudentLog.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 function StudentLog() {
+  const data = ["1", "2", "3", "4", "5"]
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const carouselInfiniteScroll = () => {
+    if (currentIndex === data.length-1) {
+      return setCurrentIndex(0)
+    }
+    return setCurrentIndex(currentIndex+1)
+  }
+
+  useEffect(()=> {
+    const interval = setInterval(()=> {carouselInfiniteScroll()}, 3000)
+    return()=> clearInterval(interval)
+  })
     return (
       <div className="studentlog card">
         Student Name
@@ -37,6 +52,12 @@ function StudentLog() {
             <p>Put in radio box for repetition or minutes</p>
           </li>
         </ul>
+        <div className="carousel-container">
+          { data.map((item, index)=> {
+            return <h1 className="carousel-item" style={{transform: `translate(-${currentIndex * 100}%)`}}
+            key={index}>{item}</h1>
+          })}
+        </div>
       </div>
     );
   }
