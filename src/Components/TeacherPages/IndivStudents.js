e85d54c6f4a36b56e7bd46a9d3fc0a97c3c523f0
Sheila Strahan <smstrahan@gmail.com>
2022-12-15 18:41:58 -0700

    cleaning up new file

--- src/Components/TeacherPages/IndivStudents.js
+++ src/Components/TeacherPages/IndivStudents.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 const IndivStudents = () => {
-    const [values, setValues] = useState({ title: "", description: "", notes: "" });
+    const [values, setValues] = useState(
+        { title: "", description: "", notes: "" }
+    );
 
     const [arrayOfAssignments, setArray] = useState([])
   
@@ -19,31 +22,33 @@ const IndivStudents = () => {
       setArray([...arrayOfAssignments])
       console.log({arrayOfAssignments})
     };
-return (
-    <div>
-<form>
-<h2>Individual Student Management</h2>
-<label htmlFor="students">Select Student</label>
-<select id="students" name="students">
-  <option value="Grace">Grace</option>
-  <option value="Alice">Alice</option>
-  <option value="Ella">Ella</option>
-  <option value="Liam">Liam</option>
-</select>
-</form>
-<div>
-<form>
-  <h2>Add Assignment</h2>
-  <label htmlFor="title">Title</label>
-  <input type="text" id="title" onChange={handleInputChange} name="title"/>
-  <label htmlFor="description">Description</label>
-  <input type="text" id="description" onChange={handleInputChange} name="description"/>
-  <label for="notes">Notes</label>
-  <input type="text" id="notes" onChange={handleInputChange} name="notes"/>
-  <button type="submit" onClick={handleSubmit}>Add New Assignment</button>
-</form>
-</div>
-</div>
-)}
+
+    return (
+        <div>
+            <form>
+                <h2>Individual Student Management</h2>
+                <label htmlFor="students">Select Student</label>
+                <select id="students" name="students">
+                    <option value="Grace">Grace</option>
+                    <option value="Alice">Alice</option>
+                    <option value="Ella">Ella</option>
+                    <option value="Liam">Liam</option>
+                </select>
+            </form>
+            <div>
+                <form>
+                    <h2>Add Assignment</h2>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" onChange={handleInputChange} name="title"/>
+                    <label htmlFor="description">Description</label>
+                    <input type="text" id="description" onChange={handleInputChange} name="description"/>
+                    <label for="notes">Notes</label>
+                    <input type="text" id="notes" onChange={handleInputChange} name="notes"/>
+                    <button type="submit" onClick={handleSubmit}>Add New Assignment</button>
+                </form>
+            </div>
+        </div>
+    )
+};
 
 export default IndivStudents;
