a828f62599c913d040e9525de4e6846b1ede6423
Sheila Strahan <smstrahan@gmail.com>
2022-12-19 17:16:12 -0800

    made reactive message to go with progress bar

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function ProgressBar() {
+    const [progressMessage, setProgressMessage] = useState(<div>Let's Get Started!</div>);
+
     let completed = 30;
 let progressWidth = {
     width: `${completed}%`}
@@ -10,15 +12,22 @@ useEffect(() => {
     console.log(`--width-progress-bar: ${width}`)
 }, [])
 
-function setWidth(width) {
+function setWidth() {
     const oldWidth = getComputedStyle(document.documentElement).getPropertyValue('--width-progress-bar').replace('%', '')
     let newWidth = parseInt(oldWidth);
     if (newWidth === 100) {
         newWidth = 0;
+            setProgressMessage(<div>Let's Get Started!</div>)
     } else {
     newWidth = newWidth + 25;
+        if (newWidth === 100) {
+            setProgressMessage(<div>YAY!</div>)
+        } else {
+    setProgressMessage(<div>Keep Going!</div>)
+        }
     }
     document.documentElement.style.setProperty('--width-progress-bar', `${newWidth}%`);
+    console.log(typeof getComputedStyle(document.documentElement).getPropertyValue('--width-progress-bar'))
 }
 
 const addToProgress = (e) => {
@@ -26,12 +35,14 @@ const addToProgress = (e) => {
     console.log('clicked')
     completed = 75
 }
+
 return (
     <>
     <div className="pb-border">
         <div className="pb-fill" id="pbfill" onClick={addToProgress}></div>
     </div>
-    <button onClick={() => setWidth('50%')}>Click for progress</button>
+    <button onClick={() => setWidth()}>Click for progress</button>
+    {progressMessage}
     </>
 )
 
