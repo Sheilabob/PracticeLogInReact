42d303c9af074abc2f544805d2111d497aac1f04
Sheila Strahan <smstrahan@gmail.com>
2022-12-19 17:21:57 -0800

    starting to make day checkboxes reactive

--- src/Components/AssignmentCard.js
+++ src/Components/AssignmentCard.js
@@ -11,6 +11,10 @@ function AssignmentCard() {
 
     const demoAssignment = new Assignment("Dummy Category", "Assignment Title", "Description", "Notes")
 
+    const handleDayCheckboxes = () => {
+        console.log('clicked a day')
+    }
+
     return (
         <div className="card assignment-card">
             <h3>{demoAssignment.category}</h3>
@@ -18,19 +22,19 @@ function AssignmentCard() {
             <p>{demoAssignment.description}</p>
             <p>{demoAssignment.notes}</p>
             <label>Monday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes}/>
             <label>Tuesday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes} />
             <label>Wednesday</label>
-            <input type="checkbox"  />
+            <input type="checkbox"  onChange={handleDayCheckboxes}/>
             <label>Thursday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes} />
             <label>Friday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes} />
             <label>Saturday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes} />
             <label>Sunday</label>
-            <input type="checkbox"  />
+            <input type="checkbox" onChange={handleDayCheckboxes} />
         </div>
     )
 };
