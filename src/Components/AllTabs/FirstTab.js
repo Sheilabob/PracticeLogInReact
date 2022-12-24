ff23bed1bd67e409c8c70f81a58cd88289c1c1d6
Sheila Strahan <smstrahan@gmail.com>
2022-12-24 08:31:07 -0800

    rendered sorted student list by day and time

--- src/Components/AllTabs/FirstTab.js
+++ src/Components/AllTabs/FirstTab.js
@@ -12,21 +12,20 @@ const FirstTab = ({students}) => {
         'Mon': 1,'Tue': 2,'Wed': 3,'Thur': 4,'Fri': 5,'Sat': 6,
         'Sun': 7
      };
-    const sortedDayList = students.sort((a, b) => {
-        return map[a.day] - map[b.day];
+    const sortedDayTimeList = students.sort((a, b) => {
+        return (map[a.day] > map[b.day]) ? 1: (map[a.day] === map[b.day]) ? ((a.time > b.time) ? 1 : -1): -1;
      });
+     const schedule = sortedDayTimeList.map( entry => <><p>{entry.day}, {entry.time} - {entry.name}</p></>)
 
-     console.log("sorted by day", sortedDayList)
-     const sortedTimeList = sortedDayList.sort((a,b) => {
-        return a.time - b.time
-     })
-     console.log("sorted by time", sortedTimeList)
+     console.log("sorted by day", sortedDayTimeList)
 
   return (
     <div className="FirstTab">
       <p>Full Roster Reports</p>
       {/* First tab content will go here */}
       Students: {studentList}
+      Schedule
+      {schedule}
     </div>
   );
 };
