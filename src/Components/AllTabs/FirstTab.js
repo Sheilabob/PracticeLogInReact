76e2828335a8cb9220e4de0a4a607686f15e721e
Sheila Strahan <smstrahan@gmail.com>
2022-12-25 07:44:45 -0800

    put roster and schedule into variables

--- src/Components/AllTabs/FirstTab.js
+++ src/Components/AllTabs/FirstTab.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const FirstTab = ({students}) => {
 
-    const studentList = students.map(
-        student => <><h2>{student.name}</h2>  
+    const studentList =<><h2>Student List</h2> {students.map(
+        student => <><h3>{student.name}</h3>  
                     <p>{student.day}, {student.time}</p>
                     <p>Grade: {student.grade}, Level: {student.level}</p>
                     </>
-    )
+    )}</>
     const map = {
         'Mon': 1,'Tue': 2,'Wed': 3,'Thur': 4,'Fri': 5,'Sat': 6,
         'Sun': 7
@@ -15,16 +15,14 @@ const FirstTab = ({students}) => {
     const sortedDayTimeList = students.sort((a, b) => {
         return (map[a.day] > map[b.day]) ? 1: (map[a.day] === map[b.day]) ? ((a.time > b.time) ? 1 : -1): -1;
      });
-     const schedule = sortedDayTimeList.map( entry => <><p>{entry.day}, {entry.time} - {entry.name}</p></>)
+     const schedule = <><h2>Schedule</h2> {sortedDayTimeList.map( entry => <><p>{entry.day}, {entry.time} - {entry.name}</p></>)}</>
 
      console.log("sorted by day", sortedDayTimeList)
 
   return (
     <div className="FirstTab">
       <p>Full Roster Reports</p>
-      {/* First tab content will go here */}
-      Students: {studentList}
-      Schedule
+      {studentList}
       {schedule}
     </div>
   );
