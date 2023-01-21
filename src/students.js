7f76fdc04ccc2346d263a453dc691db7f6a29478
Sheila Strahan <smstrahan@gmail.com>
2023-01-20 18:40:05 -0700

    finished fomatting student info array

--- src/students.js
+++ src/students.js
@@ -32,15 +32,164 @@ let students = [
             }
         ]
     }, 
-    {userName:'Ella', firstName:'Ella', lastName:'Munger', day:'Tue', time:'4:00pm', grade:'6th', level:'1B', arrayOfAssignments: [{title:"Ella's assignment", description:"Ella's assignments", notes: "Ella's notes"}, {title:"Ella's assignment2", description:"Ella's assignments2", notes: "Ella's notes2"}, {title:"Ella's assignment3", description:"Ella's assignments3", notes: "Ella's notes3"}]}, 
-    {userName:'Liam', firstName:'Liam', lastName:'Durfee',day:'Tue', time:'4:30pm', grade:'KG', level:'1A', arrayOfAssignments: []}, 
-    {userName:'Zinn', firstName:'Zinn-David', lastName:'Levine', day:'Tue', time:'5:00pm', grade:'KG', level:'1B', arrayOfAssignments: []}, 
-    {userName:'Noah', firstName:'Noah', lastName:'Kauffman', day:'Tue', time:'8:00am', grade:'5th', level:'2', arrayOfAssignments: [{title:"Noah's assignment", description:"Noah's assignments", notes: "Noah's notes", date: "10/3/2022"}]}, 
-    {userName:'Grace', firstName:'Grace', lastName: 'Birdwell', day:'Wed', time:'8:00am', grade:'7th', level:'2', arrayOfAssignments: [{title:"Grace's assignment", description:"Grace's assignments", notes: "Grace's notes", date: "10/3/2022"}], personalInfo: {phone:'435-259-0000', email:'gggg@gggg.com', parentNames:'Chelsea and Drew Birdwell', address:'111 N Road Drive', startDate:'1/6/2020'}}, 
-    {userName: 'Riv', firstName:'Riv', lastName:'McCann',  day:'Wed', time:'3:30pm', grade:'1st', level:'1A', arrayOfAssignments: [{title:"Riv's assignment", description:"Riv's assignments", notes: "Riv's notes", date: "10/3/2022"}]}, 
-    {userName:'Estelle', firstName:'Estelle', lastName:'Surkes', day:'Wed', time:'4:00pm', grade:'5th', level:'1B', arrayOfAssignments: [{title:"Estelle's assignment", description:"Estelle's assignments", notes: "Estelle's notes", date: "10/3/2022"}]}, 
-    {userName: 'Azalea', firstName:'Azalea', lastName:'Han', day:'Wed', time:'4:30pm', grade:'3rd', level:'1A', arrayOfAssignments: [{title:"Azalea's assignment", description:"Azalea's assignments", notes: "Azalea's notes", date: "10/3/2022"}]}, 
-    {userName:'Inga', firstName:'Inga', lastName:'Wilkowski', day:'Wed', time:'5:00pm', grade:'7th', level:'1B', arrayOfAssignments: [{title:"Inga's assignment", description:"Inga's assignments", notes: "Inga's notes", date: "10/3/2022"}]}, 
-    {userName:'Miles', firstName:'Miles', lastName:'Topper', day:'Wed', time:'5:30pm', grade:'5th', level:'2', arrayOfAssignments: [{title:"Miles' assignment", description:"Miles' assignments", notes: "Miles' notes", date: "10/3/2022"}]}]
+    {   userName:'Ella', 
+        firstName:'Ella', 
+        lastName:'Munger', 
+        day:'Tue', 
+        time:'4:00pm', 
+        grade:'6th', 
+        level:'1B', 
+        arrayOfAssignments: [
+            {   title:"Ella's assignment", 
+                description:"Ella's assignments", 
+                notes: "Ella's notes"
+            }, 
+            {   title:"Ella's assignment2", 
+                description:"Ella's assignments2", 
+                notes: "Ella's notes2"
+            }, 
+            {   title:"Ella's assignment3", 
+                description:"Ella's assignments3", 
+                notes: "Ella's notes3"
+            }
+        ]
+    }, 
+    {   userName:'Liam', 
+        firstName:'Liam', 
+        lastName:'Durfee',
+        day:'Tue', 
+        time:'4:30pm', 
+        grade:'KG', 
+        level:'1A', 
+        arrayOfAssignments: [
+
+        ]
+    }, 
+    {
+        userName:'Zinn', 
+        firstName:'Zinn-David', 
+        lastName:'Levine', 
+        day:'Tue', 
+        time:'5:00pm', 
+        grade:'KG', 
+        level:'1B', 
+        arrayOfAssignments: [
+
+        ]
+    }, 
+    {
+        userName:'Noah', 
+        firstName:'Noah', 
+        lastName:'Kauffman', 
+        day:'Tue', 
+        time:'8:00am', 
+        grade:'5th', 
+        level:'2', 
+        arrayOfAssignments: [
+            {   title:"Noah's assignment", 
+                description:"Noah's assignments", 
+                notes: "Noah's notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }, 
+    {   userName:'Grace', 
+        firstName:'Grace', 
+        lastName: 'Birdwell', 
+        day:'Wed', 
+        time:'8:00am', 
+        grade:'7th', 
+        level:'2', 
+        arrayOfAssignments: [
+            {   title:"Grace's assignment", 
+                description:"Grace's assignments", 
+                notes: "Grace's notes", 
+                date: "10/3/2022"
+            }
+        ], 
+        personalInfo: 
+        {   phone:'435-259-0000', 
+            email:'gggg@gggg.com', 
+            parentNames:'Chelsea and Drew Birdwell', 
+            address:'111 N Road Drive', 
+            startDate:'1/6/2020'
+        }
+    }, 
+    {   userName: 'Riv', 
+        firstName:'Riv', 
+        lastName:'McCann',  
+        day:'Wed', 
+        time:'3:30pm', 
+        grade:'1st', 
+        level:'1A', 
+        arrayOfAssignments: [
+            {   title:"Riv's assignment", 
+                description:"Riv's assignments", 
+                notes: "Riv's notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }, 
+    {   userName:'Estelle', 
+        firstName:'Estelle', 
+        lastName:'Surkes', 
+        day:'Wed', 
+        time:'4:00pm', 
+        grade:'5th', 
+        level:'1B', 
+        arrayOfAssignments: [
+            {   title:"Estelle's assignment", 
+                description:"Estelle's assignments", 
+                notes: "Estelle's notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }, 
+    {   userName: 'Azalea', 
+        firstName:'Azalea',
+        lastName:'Han', 
+        day:'Wed', 
+        time:'4:30pm', 
+        grade:'3rd', 
+        level:'1A', 
+        arrayOfAssignments: [
+            {   title:"Azalea's assignment", 
+                description:"Azalea's assignments", 
+                notes: "Azalea's notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }, 
+    {   userName:'Inga', 
+        firstName:'Inga', 
+        lastName:'Wilkowski', 
+        day:'Wed', 
+        time:'5:00pm', 
+        grade:'7th', 
+        level:'1B', 
+        arrayOfAssignments: [
+            {   title:"Inga's assignment", 
+                description:"Inga's assignments", 
+                notes: "Inga's notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }, 
+    {   userName:'Miles', 
+        firstName:'Miles', 
+        lastName:'Topper', 
+        day:'Wed', 
+        time:'5:30pm', 
+        grade:'5th', 
+        level:'2', 
+        arrayOfAssignments: [
+            {   title:"Miles' assignment", 
+                description:"Miles' assignments", 
+                notes: "Miles' notes", 
+                date: "10/3/2022"
+            }
+        ]
+    }
+]
 
 export default students;
